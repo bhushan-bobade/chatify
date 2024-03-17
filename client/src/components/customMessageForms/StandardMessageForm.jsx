f587f3d9347d5ff85c5cb26405858efc23a915f6
greatbb712@gmail.com <greatbb712@gmail.com>
2024-03-17 12:55:36 +0530

    commit

---
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import MessageFormUI from "./MessageFormUI";
+
+const StandardMessageForm = ({ props, activeChat }) => {
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+
+  const handleChange = (e) => setMessage(e.target.value);
+
+  const handleSubmit = async () => {
+    if (!activeChat) {
+    console.error("Active chat is undefined");
+    return;
+  }
+    const date = new Date()
+      .toISOString()
+      .replace("T", " ")
+      .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+    const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+    const form = {   //All below const form information are from REST API
+      attachments: at,  
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    };
+
+    props.onSubmit(form);
+    setMessage("");
+    setAttachment("");
+  };
+
+  return (
+    <MessageFormUI
+      setAttachment={setAttachment}
+      message={message}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  );
+};
+
+export default StandardMessageForm;
